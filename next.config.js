e7075de099817b639155903cff53cd9ef389427a
Matt Strom <matthew.strom@evolv.ai>
2023-06-27 12:17:46 -0700

    Configure NextJS app to use Preact

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPreact = require("next-plugin-preact");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+// 	webpack: (config, { dev, isServer}) => {
+// 		if (!dev && !isServer) {
+// 			Object.assign(config.resolve.alias, {
+// 				"react/jsx-runtime.js": "preact/compat/jsx-runtime",
+// 				react: "preact/compat",
+// 				"react-dom/test-utils": "preact/test-utils",
+// 				"react-dom": "preact/compat",
+// 			});
+// 		}
+// 		return config;
+// 	}
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPreact({});
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = (props: any) => {
+	return (
+		<>
+			<div>Hello</div>
+		</>
+	);
+};
+
+export default Home;
